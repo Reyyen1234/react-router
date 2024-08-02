878d8d850f51ec17859d5731a817676724eaf23d
Reyyen cuneyt <rrayyan213@gmail.com>
2024-08-02 09:21:41 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App() { 
+  return ( 
+    <Router>
+      <div>
+        <nav> 
+          <ul>
+            <li><Link to="/">Home</Link></li> 
+            <li><Link to="/about">About</Link></li> 
+            <li><Link to="/users">Users</Link></li> 
+          </ul> 
+        </nav> 
+        {/*Routes: A element looks through its children elements and renders the first one that matches the current URL. */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/users" element={<Users />} />
+        </Routes>
+      </div> 
+    </Router>
+  ); 
 }
+
+function Home() { 
+  return <h2>Home</h2>; } 
+  
+function About() { 
+  return <h2>About</h2>; } 
+  
+function Users() { 
+  return <h2>Users</h2>; }
 
 export default App;
